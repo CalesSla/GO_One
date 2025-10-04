3544428cae39d9c2707318b2f657a851b8465973
Slava Calestru <veaceslavcalestru@gmail.com>
2025-10-04 19:41:52 +0200

    profit calculator update

--- exercises/profit_calculator.go
+++ exercises/profit_calculator.go
@@ -1,13 +1,55 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+)
+
+const dataFile = "data.txt"
+
+func writeToFile(ebt, profit, ratio float64) {
+	data := fmt.Sprintf("%v,%v,%v", ebt, profit, ratio)
+	fmt.Println("Writing to file:", data)
+	err := os.WriteFile(dataFile, []byte(data), 0644)
+	if err != nil {
+		panic("Unable to write data to file.")
+	}
+}
+
+func readFromFile(filePath string) (revenue, expenses, taxRate float64) {
+	fmt.Println("Reading from file...")
+	content, err := os.ReadFile(filePath)
+	if err != nil {
+		panic("Unable to read data from file.")
+	}
+	_, err = fmt.Sscanf(string(content), "%f,%f,%f", &revenue, &expenses, &taxRate)
+	if err != nil {
+		panic("Unable to parse data from file.")
+	}
+
+	fmt.Println("Revenue: ", revenue)
+	fmt.Println("Expenses: ", expenses)
+	fmt.Println("Tax Rate: ", taxRate)
+
+	return revenue, expenses, taxRate
+}
 
 func main() {
-	revenue := getUserInput("Revenue: ")
-	expenses := getUserInput("Expenses: ")
-	taxRate := getUserInput("Tax Rate (%): ")
+	var revenue, expenses, taxRate float64
+
+	_, err := os.Stat(dataFile)
+	if os.IsNotExist(err) {
+		revenue = getUserInput("Revenue: ")
+		expenses = getUserInput("Expenses: ")
+		taxRate = getUserInput("Tax Rate (%): ")
+	} else {
+		revenue, expenses, taxRate = readFromFile(dataFile)
+	}
 
 	ebt, profit, ratio := calculateFinancials(revenue, expenses, taxRate)
+
+	writeToFile(ebt, profit, ratio)
 
 	printStats(ebt, profit, ratio)
 
@@ -17,8 +59,20 @@ func getUserInput(infoText string) (result float64) {
 
 	fmt.Print(infoText)
 	fmt.Scan(&result)
-
+	err := validateInput(result)
+	if err != nil {
+		return getUserInput(infoText)
+	}
 	return result
+}
+
+func validateInput(input float64) (err error) {
+	if input <= 0 {
+		err = errors.New("Invalid amount. Please enter a positive number.\n")
+		fmt.Print(err)
+		return err
+	}
+	return nil
 }
 
 func calculateFinancials(
