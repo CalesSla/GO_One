acd9e0506aa001e51b8eb09dd846cd0293555d32
Slava Calestru <veaceslavcalestru@gmail.com>
2025-10-05 09:55:48 +0200

    extra go packages

--- control_structures/bank.go
+++ control_structures/bank.go
@@ -1,40 +1,18 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"os"
-	"strconv"
+
+	"example.com/bank/fileops"
+	"github.com/Pallinder/go-randomdata"
 )
 
 const accountBalanceFile = "balance.txt"
 
-func getBalanceFromFile() (float64, error) {
-	data, err := os.ReadFile(accountBalanceFile)
-
-	if err != nil {
-		return 1000, errors.New("Failed to find balance file.")
-	}
-
-	balanceText := string(data)
-	balance, err := strconv.ParseFloat(balanceText, 64)
-
-	if err != nil {
-		return 1000, errors.New("Failed to parse stored balance value.")
-	}
-
-	return balance, nil
-}
-
-func wrtiteBalanceToFile(balance float64) {
-	balanceText := fmt.Sprint(balance)
-	os.WriteFile(accountBalanceFile, []byte(balanceText), 0644)
-}
-
 func main() {
 	var accountBalance float64
 	var err error
-	accountBalance, err = getBalanceFromFile()
+	accountBalance, err = fileops.GetFloatFromFile(accountBalanceFile)
 
 	if err != nil {
 		fmt.Println("ERROR")
@@ -44,14 +22,11 @@ func main() {
 	}
 
 	fmt.Println("Welcome to Go Bank!")
+	fmt.Println("Reach us 24/7 at: ", randomdata.PhoneNumber())
 
 	for {
 
-		fmt.Println("What do you want to do today?")
-		fmt.Println("1. Check Balance")
-		fmt.Println("2. Deposit")
-		fmt.Println("3. Withdraw")
-		fmt.Println("4. Exit")
+		presentOptions()
 
 		var choice int
 		fmt.Print("Your choice: ")
@@ -71,7 +46,7 @@ func main() {
 			}
 			accountBalance += depositAmount
 			fmt.Println("Balance updated! New amount: ", accountBalance)
-			wrtiteBalanceToFile(accountBalance)
+			fileops.WrtiteFloatToFile(accountBalance, accountBalanceFile)
 		case 3:
 			fmt.Print("Your withdraw: ")
 			var withdrawAmount float64
@@ -88,7 +63,7 @@ func main() {
 
 			accountBalance -= withdrawAmount
 			fmt.Println("Balance updated! New amount: ", accountBalance)
-			wrtiteBalanceToFile(accountBalance)
+			fileops.WrtiteFloatToFile(accountBalance, accountBalanceFile)
 		case 4:
 			fmt.Println("Goodbye!")
 			return
@@ -96,44 +71,6 @@ func main() {
 			fmt.Println("Invalid choice. Please select a valid option.")
 			break
 		}
-
-		// wantsCheckBalance := choice == 1
-
-		// if choice == 1 {
-		// 	fmt.Println("Your balance is: ", accountBalance)
-		// } else if choice == 2 {
-		// 	fmt.Print("Your deposit: ")
-		// 	var depositAmount float64
-		// 	fmt.Scan(&depositAmount)
-
-		// 	if depositAmount <= 0 {
-		// 		fmt.Println("Invalid amount. Please enter a positive number.")
-		// 		continue
-		// 	}
-
-		// 	accountBalance += depositAmount
-		// 	fmt.Println("Balance updated! New amount: ", accountBalance)
-		// } else if choice == 3 {
-		// 	fmt.Print("Your withdraw: ")
-		// 	var withdrawAmount float64
-		// 	fmt.Scan(&withdrawAmount)
-
-		// 	if withdrawAmount <= 0 {
-		// 		fmt.Println("Invalid amount. Please enter a positive number.")
-		// 		continue
-		// 	}
-		// 	if withdrawAmount > accountBalance {
-		// 		fmt.Println("You can't withdraw more than you have.")
-		// 		continue
-		// 	}
-
-		// 	accountBalance -= withdrawAmount
-		// 	fmt.Println("Balance updated! New amount: ", accountBalance)
-		// } else {
-		// 	fmt.Println("Goodbye!")
-		// 	// return
-		// 	break
-		// }
 
 		fmt.Println("Your choice: ", choice)
 		fmt.Println("Thank you for using Go Bank!")
