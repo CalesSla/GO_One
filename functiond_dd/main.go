0bbbed5bee17a294c7c29e1c1ad600a5dc461c5e
Slava Calestru <veaceslavcalestru@gmail.com>
2025-10-07 09:02:08 +0200

    closures

--- functiond_dd/main.go
+++ functiond_dd/main.go
@@ -4,15 +4,28 @@ import (
 	"fmt"
 )
 
+type transformFn func(int) int
+
 func main() {
 	numbers := []int{1, 2, 3, 4, 5}
 
-	transformed := transformNumbers(&numbers)
+	double := createTransformer(2)
+	triple := createTransformer(3)
+
+	transformed := transformNumbers(&numbers, func(number int) int {
+		return number * 2
+	})
+
+	doubled := transformNumbers(&numbers, double)
+	tripled := transformNumbers(&numbers, triple)
+
+	fmt.Println(doubled)
+	fmt.Println(tripled)
 
 	fmt.Println(transformed)
 }
 
-func transformNumbers(numbers *[]int, transform func(int) int) []int {
+func transformNumbers(numbers *[]int, transform transformFn) []int {
 	dNumbers := []int{}
 
 	for _, val := range *numbers {
@@ -20,4 +33,10 @@ func transformNumbers(numbers *[]int, transform func(int) int) []int {
 	}
 
 	return dNumbers
+}
+
+func createTransformer(factor int) func(int) int {
+	return func(number int) int {
+		return number * factor
+	}
 }
