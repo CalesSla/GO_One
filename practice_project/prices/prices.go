0ba13bc6d196a8a77268763493241ee632722cb3
Slava Calestru <veaceslavcalestru@gmail.com>
2025-10-08 13:28:18 +0200

    pp complete

--- practice_project/prices/prices.go
+++ practice_project/prices/prices.go
@@ -1,62 +1,48 @@
 package prices
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"strconv"
+
+	"example.com/practice_project/conversion"
+	"example.com/practice_project/iomanager"
 )
 
 type TaxIncludedPriceJob struct {
-	TaxRate           float64
-	InputPrices       []float64
-	TaxIncludedPrices map[string]float64
+	IOManager         iomanager.IOManager `json:"-"`
+	TaxRate           float64             `json:"tax_rate"`
+	InputPrices       []float64           `json:"input_prices"`
+	TaxIncludedPrices map[string]string   `json:"tax_included_prices"`
 }
 
-func (jon *TaxIncludedPriceJob) LoadData() {
-	file, err := os.Open("prices.txt")
-	if err != nil {
-		fmt.Println("Could not open file!")
-		fmt.Println(err)
-		return
-	}
+func (job *TaxIncludedPriceJob) LoadData() error {
 
-	scanner := bufio.NewScanner(file)
-
-	lines := []string{}
-
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
-
-	err = scanner.Err()
+	lines, err := job.IOManager.ReadLines()
 
 	if err != nil {
-		fmt.Println("Reading a file content failed!")
 		fmt.Println(err)
-		file.Close()
-		return
+		return err
 	}
 
-	prices := make([]float64, len(lines))
+	prices, err := conversion.StringsToFloats(lines)
 
-	for lineIndex, line := range lines {
-		floatPrice, err := strconv.ParseFloat(line, 64)
-
-		if err != nil {
-			fmt.Println("Converting price to float failed!")
-			fmt.Println(err)
-			file.Close()
-			return
-		}
-		prices[lineIndex] = floatPrice
+	if err != nil {
+		fmt.Println("Converting price to float failed!")
+		fmt.Println(err)
+		return err
 	}
+	job.InputPrices = prices
 
-	jon.InputPrices = prices
+	return nil
 }
 
-func (job *TaxIncludedPriceJob) Process() {
-	job.LoadData()
+func (job *TaxIncludedPriceJob) Process() error {
+
+	err := job.LoadData()
+
+	if err != nil {
+		return err
+	}
+
 	result := make(map[string]string)
 
 	for _, price := range job.InputPrices {
@@ -64,11 +50,14 @@ func (job *TaxIncludedPriceJob) Process() {
 		result[fmt.Sprintf("%.2f", price)] = fmt.Sprintf("%.2f", taxIncludedPrice)
 	}
 
-	fmt.Println(result)
+	job.TaxIncludedPrices = result
+
+	return job.IOManager.WriteResult(job)
 }
 
-func NewTaxIncludedPriceJob(taxRate float64) *TaxIncludedPriceJob {
+func NewTaxIncludedPriceJob(iom iomanager.IOManager, taxRate float64) *TaxIncludedPriceJob {
 	return &TaxIncludedPriceJob{
+		IOManager:   iom,
 		InputPrices: []float64{10, 20, 30},
 		TaxRate:     taxRate,
 	}
