f59d75ac55f2a6e5b01d8ad0bdceb2ab1991aa0f
Slava Calestru <veaceslavcalestru@gmail.com>
2025-10-08 22:18:59 +0200

    naming conventions

---
+++ Go_96/basics/basics.go
@@ -0,0 +1,20 @@
+package basics
+
+import "fmt"
+
+var middleName = "Cane"
+
+func main() {
+	// var age int
+	// var name string = "John"
+	// var name1 = "Jane"
+	// printname()
+	fmt.Println(middleName)
+	printname()
+
+}
+
+func printname() {
+	firstName := "Michael"
+	fmt.Println(firstName)
+}
