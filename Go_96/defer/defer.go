72a54f5fe1d19f2137a46b6d51b9454ebabdbfa7
veaceslavcalestru@gmail.com <veaceslavcalestru@gmail.com>
2025-10-10 17:34:36 +0200

    feat: update process function to accept an integer parameter and demonstrate defer behavior with error handling

--- Go_96/defer/defer.go
+++ Go_96/defer/defer.go
@@ -4,12 +4,18 @@ import "fmt"
 
 func main() {
 
-	process()
+	process(10)
 }
 
-func process() {
+func process(i int) {
+	defer fmt.Println("Deferred i value: ", i)
 	defer fmt.Println("Deferred: This will run first")
 	defer fmt.Println("Deferred: This will run second")
 	defer fmt.Println("Deferred: This will run third")
 	fmt.Println("This will run first")
+	i++
+	fmt.Println("Value of i:", i)
+	// exit with an error
+	panic("An error occurred!")
+	fmt.Println("This will run last")
 }
