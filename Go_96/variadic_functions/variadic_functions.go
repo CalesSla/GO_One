e1353fa2b28dd40c8f2e5c457c2919806f587bbb
veaceslavcalestru@gmail.com <veaceslavcalestru@gmail.com>
2025-10-10 15:28:46 +0200

    feat: implement sum function with variadic parameters and update main function to demonstrate usage

--- Go_96/variadic_functions/variadic_functions.go
+++ Go_96/variadic_functions/variadic_functions.go
@@ -1,9 +1,24 @@
 package main
 
+import "fmt"
+
 func main() {
+	sequence, total := sum(1, 20, 30, 40)
+	fmt.Println("Sequence:", sequence, "Total:", total)
 
+	sequence2, total2 := sum(2, 5, 10)
+	fmt.Println("Sequence:", sequence2, "Total:", total2)
 
+	numbers := []int{1, 2, 3, 4, 5}
+	sequence3, total3 := sum(3, numbers...)
+	fmt.Println("Sequence:", sequence3, "Total:", total3)
 
 }
 
-func 
+func sum(sequence int, numbers ...int) (int, int) {
+	total := 0
+	for _, v := range numbers {
+		total += v
+	}
+	return sequence, total
+}
