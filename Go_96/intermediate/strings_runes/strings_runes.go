26c9a1592fb823f97aff64e92a681e07edf1aced
veaceslavcalestru@gmail.com <veaceslavcalestru@gmail.com>
2025-10-12 21:20:37 +0200

    feat: add examples for string comparison and rune handling in Go

--- Go_96/intermediate/strings_runes/strings_runes.go
+++ Go_96/intermediate/strings_runes/strings_runes.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unicode/utf8"
+)
 
 func main() {
 
@@ -23,4 +26,46 @@ func main() {
 	name := "Alice"
 
 	fmt.Println(greeting + name)
+
+	str1 := "Apple"
+	str := "apple"
+	str2 := "Banana"
+	str3 := "Apple"
+	fmt.Println(str1 < str2)
+	fmt.Println(str3 < str1)
+	fmt.Println(str > str1)
+
+	for _, char := range message {
+		// fmt.Printf("Character at index %d is %c\n", i, char)
+		fmt.Printf("%v\n", char)
+	}
+
+	fmt.Println("Rune count: ", utf8.RuneCountInString(greeting))
+
+	greetingWithName := greeting + name
+	fmt.Println(greetingWithName)
+
+	var ch rune = 'a'
+	var strCh string = "a"
+	fmt.Println(ch)
+	fmt.Println(strCh)
+
+	fmt.Printf("%c\n", ch)
+	fmt.Printf("%v\n", strCh)
+
+	cstr := string(ch)
+	fmt.Println(cstr)
+
+	fmt.Printf("Type of ch is %T\n", ch)
+	const NIHONGO = "日本語"
+	fmt.Println(NIHONGO)
+
+	jhello := "こんにちは"
+	for _, runeValue := range jhello {
+		fmt.Printf("%c\n", runeValue)
+	}
+
+	// Smile
+	smile := '😊'
+	fmt.Println(smile)
 }
