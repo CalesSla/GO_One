f1e0d9f0f28f6c49cbc0d513e49e86f415edd2a2
veaceslavcalestru@gmail.com <veaceslavcalestru@gmail.com>
2025-10-12 20:37:28 +0200

    feat: enhance pointer example with nil check and value modification

--- Go_96/intermediate/pointers/pointers.go
+++ Go_96/intermediate/pointers/pointers.go
@@ -5,11 +5,23 @@ import "fmt"
 func main() {
 
 	var ptr *int
+	var ptr2 *int
 	var a int = 10
 
 	ptr = &a
 
 	fmt.Println(a)
 	fmt.Println(ptr)
+	fmt.Println(*ptr)
 
+	if ptr2 == nil {
+		fmt.Println("ptr2 is nil")
+	}
+
+	modifyValue(ptr)
+	fmt.Println(a)
+}
+
+func modifyValue(ptr *int) {
+	*ptr++
 }
