a4e72d9ebae5535ed5c476ffdebd5d3459450487
veaceslavcalestru@gmail.com <veaceslavcalestru@gmail.com>
2025-10-13 13:07:41 +0200

    feat: add examples for fmt package and formatting verbs in Go

---
+++ Go_96/intermediate/formatting_verbs/formatting_verbs.go
@@ -0,0 +1,54 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// i := 111_111_111_505.5
+	// str := "Hello World"
+
+	// fmt.Println(i)
+	// fmt.Println(str)
+
+	// fmt.Printf("%v\n", i)
+	// fmt.Printf("%#v\n", i)
+	// fmt.Printf("%T\n", i)
+	// fmt.Printf("%v%%\n", i)
+
+	// fmt.Printf("%v\n", str)
+	// fmt.Printf("%#v\n", str)
+	// fmt.Printf("%T\n", str)
+
+	// myint := 18
+	// fmt.Printf("%b\n", myint)  // binary
+	// fmt.Printf("%d\n", myint)  // decimal
+	// fmt.Printf("%+d\n", myint) // decimal with sign
+	// fmt.Printf("%o\n", myint)  // octal
+	// fmt.Printf("%x\n", myint)  // hex with lowercase letters
+	// fmt.Printf("%X\n", myint)  // hex with uppercase letters
+	// fmt.Printf("%#x\n", myint) // hex with 0x prefix and lowercase letters
+	// fmt.Printf("%4d\n", myint) // width 4, right aligned
+
+	// txt := "World"
+	// fmt.Printf("%s\n", txt)
+	// fmt.Printf("%q\n", txt)
+	// fmt.Printf("%8s\n", txt)
+	// fmt.Printf("%-8s\n", txt)
+	// fmt.Printf("%x\n", txt)
+	// fmt.Printf("% x\n", txt)
+
+	// t := true
+	// f := false
+
+	// fmt.Printf("%t\n", t)
+	// fmt.Printf("%t\n", f)
+
+	flt := 9.18
+
+	fmt.Printf("%e\n", flt)
+	fmt.Printf("%f\n", flt)
+	fmt.Printf("%.2f\n", flt)
+	fmt.Printf("%6.2f\n", flt)
+	fmt.Printf("%g\n", flt)
+
+}
