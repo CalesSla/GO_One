306eb078d958fe2ae749ba0867010f080febdde7
veaceslavcalestru@gmail.com <veaceslavcalestru@gmail.com>
2025-10-13 13:58:59 +0200

    feat: implement age validation function and update fmt examples to comments

--- Go_96/intermediate/fmt_package/fmt_package.go
+++ Go_96/intermediate/fmt_package/fmt_package.go
@@ -4,28 +4,53 @@ import "fmt"
 
 func main() {
 
-	fmt.Print("Hello")
-	fmt.Print(" World")
-	fmt.Print(12, 456)
+	// fmt.Print("Hello")
+	// fmt.Print(" World")
+	// fmt.Print(12, 456)
 
-	fmt.Println("Hello")
-	fmt.Println("World")
-	fmt.Println(12, 456)
+	// fmt.Println("Hello")
+	// fmt.Println("World")
+	// fmt.Println(12, 456)
 
-	name := "John"
-	age := 25
-	fmt.Printf("Name: %s, Age: %d\n", name, age)
-	fmt.Printf("Binary: %b, Hex: %X\n", age, age)
+	// name := "John"
+	// age := 25
+	// fmt.Printf("Name: %s, Age: %d\n", name, age)
+	// fmt.Printf("Binary: %b, Hex: %X\n", age, age)
 
-	s := fmt.Sprint("Hello", "World!", 123, 456)
-	fmt.Print(s)
+	// s := fmt.Sprint("Hello", "World!", 123, 456)
+	// fmt.Print(s)
 
-	s = fmt.Sprintln("Hello ", "World!", 123, 456)
-	fmt.Print(s)
-	fmt.Print(s)
+	// s = fmt.Sprintln("Hello ", "World!", 123, 456)
+	// fmt.Print(s)
+	// fmt.Print(s)
 
-	sf := fmt.Sprintf("Name: %s, Age %d", name, age)
-	fmt.Print(sf)
-	fmt.Print(sf)
+	// sf := fmt.Sprintf("Name: %s, Age %d", name, age)
+	// fmt.Println(sf)
+	// fmt.Println(sf)
 
+	// var name string
+	// var age int
+
+	// fmt.Print("Enter your name and age: ")
+	// fmt.Scanln(&name, &age)
+	// fmt.Printf("Name: %s, Age: %d\n", name, age)
+
+	// fmt.Scanf("%s %d", &name, &age)
+	// fmt.Printf("Name: %s, Age: %d\n", name, age)
+
+	var age int = 15
+
+	err := checkAge(&age)
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
+	fmt.Println(err)
+}
+
+func checkAge(age *int) error {
+	if *age < 18 {
+		return fmt.Errorf("Age %d is below the allowed limit", *age)
+	}
+
+	return nil
 }
