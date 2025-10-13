a8a7a94335ed3c7124b543df1aa193695a97ce1d
veaceslavcalestru@gmail.com <veaceslavcalestru@gmail.com>
2025-10-13 13:43:47 +0200

    feat: add additional examples for formatted output using fmt package

--- Go_96/intermediate/fmt_package/fmt_package.go
+++ Go_96/intermediate/fmt_package/fmt_package.go
@@ -8,9 +8,24 @@ func main() {
 	fmt.Print(" World")
 	fmt.Print(12, 456)
 
+	fmt.Println("Hello")
+	fmt.Println("World")
+	fmt.Println(12, 456)
+
 	name := "John"
 	age := 25
 	fmt.Printf("Name: %s, Age: %d\n", name, age)
 	fmt.Printf("Binary: %b, Hex: %X\n", age, age)
+
+	s := fmt.Sprint("Hello", "World!", 123, 456)
+	fmt.Print(s)
+
+	s = fmt.Sprintln("Hello ", "World!", 123, 456)
+	fmt.Print(s)
+	fmt.Print(s)
+
+	sf := fmt.Sprintf("Name: %s, Age %d", name, age)
+	fmt.Print(sf)
+	fmt.Print(sf)
 
 }
