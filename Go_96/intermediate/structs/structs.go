a96052f2ef8efbd80747f37eb8bd6de3050773fd
veaceslavcalestru@gmail.com <veaceslavcalestru@gmail.com>
2025-10-13 14:17:37 +0200

    feat: add Person and Address structs with methods for full name and age increment

---
+++ Go_96/intermediate/structs/structs.go
@@ -0,0 +1,73 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	p := Person{
+		firstName: "John",
+		lastName:  "Doe",
+		age:       30,
+		address: Address{
+			city:    "New York",
+			country: "USA",
+		},
+	}
+
+	p1 := Person{
+		firstName: "Jane",
+		age:       25,
+	}
+
+	p1.address.city = "Los Angeles"
+	p1.address.country = "USA"
+
+	fmt.Println(p.firstName)
+	fmt.Println(p1.firstName)
+
+	fmt.Println(p.lastName)
+	fmt.Println(p1.lastName)
+
+	fmt.Println(p.fullName())
+
+	user := struct {
+		username string
+		email    string
+	}{
+		username: "user123",
+		email:    "pseudoemail@example.com",
+	}
+
+	fmt.Println(user.username)
+
+	fmt.Println("Before incrementing age:", p1.age)
+	p1.incrementAgeBy1()
+	fmt.Println("After incrementing age:", p1.age)
+
+	fmt.Println(p.address.city)
+	fmt.Println(p1.address.city)
+
+	fmt.Println(p.address.country)
+	fmt.Println(p1.address.country)
+
+}
+
+type Person struct {
+	firstName string
+	lastName  string
+	age       int
+	address   Address
+}
+
+type Address struct {
+	city    string
+	country string
+}
+
+func (p Person) fullName() string {
+	return p.firstName + " " + p.lastName
+}
+
+func (p *Person) incrementAgeBy1() {
+	p.age++
+}
