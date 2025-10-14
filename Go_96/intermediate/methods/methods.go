48579a1de1a5a37ada649180614a7f54d773294e
veaceslavcalestru@gmail.com <veaceslavcalestru@gmail.com>
2025-10-14 09:50:07 +0200

    feat: add Scale method to Rectangle and MyInt type with IsPositive and welcomeMessage methods

--- Go_96/intermediate/methods/methods.go
+++ Go_96/intermediate/methods/methods.go
@@ -11,8 +11,34 @@ func (r Rectangle) Area() float64 {
 	return r.length * r.width
 }
 
+func (r *Rectangle) Scale(factor float64) {
+	r.length *= factor
+	r.width *= factor
+}
+
 func main() {
 	rect := Rectangle{length: 10, width: 9}
 	area := rect.Area()
 	fmt.Println("Area: ", area)
+
+	rect.Scale(1.5)
+	newArea := rect.Area()
+	fmt.Println("New Area: ", newArea)
+
+	num := MyInt(-5)
+	fmt.Println("Is Positive: ", num.IsPositive())
+	num2 := MyInt(10)
+	fmt.Println("Is Positive: ", num2.IsPositive())
+
+	fmt.Println(num.welcomeMessage())
+}
+
+type MyInt int
+
+func (m MyInt) IsPositive() bool {
+	return m > 0
+}
+
+func (MyInt) welcomeMessage() string {
+	return "Welcome to Go methods!"
 }
