aca5d5081dcfabd0b5147212f9e37e4e83f3aef7
veaceslavcalestru@gmail.com <veaceslavcalestru@gmail.com>
2025-10-14 13:38:16 +0200

    feat: implement error handling with custom error type and basic math functions

---
+++ Go_96/intermediate/errors/errors.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func sqrt(x float64) (float64, error) {
+	if x < 0 {
+		return 0, errors.New("math: square root of negative number")
+	}
+	return 1, nil
+}
+
+func process(data []byte) error {
+	if len(data) == 0 {
+		return errors.New("Empty data")
+	}
+	return nil
+}
+
+func main() {
+	// result, err := sqrt(16)
+	// if err != nil {
+	// 	fmt.Println("Error:", err.Error())
+	// 	return
+	// } else {
+	// 	fmt.Println("Result:", result)
+	// }
+	// result1, err1 := sqrt(-16)
+	// if err1 != nil {
+	// 	fmt.Println("Error:", err1.Error())
+	// 	return
+	// } else {
+	// 	fmt.Println("Result:", result1)
+	// }
+
+	// data := []byte{}
+	// err := process(data)
+	// if err != nil {
+	// 	fmt.Println("Error:", err)
+	// 	return
+	// }
+	// fmt.Println("Data processed successfully")
+
+	err1 := eprocess()
+	if err1 != nil {
+		fmt.Println(err1)
+		return
+	}
+}
+
+type myError struct {
+	message string
+}
+
+func (m *myError) Error() string {
+	return fmt.Sprintf("Error: %s", m.message)
+}
+
+func eprocess() error {
+	return &myError{"Custom error message"}
+}
