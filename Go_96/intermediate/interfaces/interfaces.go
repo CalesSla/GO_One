e5a784104d1e328758e2cb0201f627262b5814be
veaceslavcalestru@gmail.com <veaceslavcalestru@gmail.com>
2025-10-14 12:37:59 +0200

    feat: implement geometry interface with rect and circle types, and add measurement functionality

--- Go_96/intermediate/interfaces/interfaces.go
+++ Go_96/intermediate/interfaces/interfaces.go
@@ -1,8 +1,89 @@
 package main
 
+import (
+	"fmt"
+	"math"
+)
+
+type geometry interface {
+	area() float64
+	perim() float64
+}
+
+type rect struct {
+	width, height float64
+}
+
+func (r rect) area() float64 {
+	return r.width * r.height
+}
+
+func (r rect) perim() float64 {
+	return 2*r.width + 2*r.height
+}
+
+type rect1 struct {
+	width, height float64
+}
+
+func (r rect1) area() float64 {
+	return r.width * r.height
+}
+
+func (r rect1) perim() float64 {
+	return 2*r.width + 2*r.height
+}
+
+type circle struct {
+	radius float64
+}
+
+func (c circle) area() float64 {
+	return math.Pi * c.radius * c.radius
+}
+
+func (c circle) perim() float64 {
+	return 2 * math.Pi * c.radius
+}
+
+func (c circle) dimeter() float64 {
+	return 2 * c.radius
+}
+
+func measure(g geometry) {
+	fmt.Println(g)
+	fmt.Println(g.area())
+	fmt.Println(g.perim())
+}
+
 func main() {
+	r := rect{width: 3, height: 4}
+	c := circle{radius: 5}
+	// r1 := rect1{width: 3, height: 4}
 
+	measure(r)
+	measure(c)
+	// measure(r1)
 
+	myPrinter(1, "John", 3.14, true)
 
 }
-g
+
+func value(i interface{}) {
+	switch i.(type) {
+	case int:
+		fmt.Println("Type: Int")
+	case string:
+		fmt.Println("Type: String")
+	case float64:
+		fmt.Println("Type: Float64")
+	default:
+		fmt.Println("Unknown Type")
+	}
+}
+
+func myPrinter(i ...interface{}) {
+	for _, v := range i {
+		fmt.Println(v)
+	}
+}
