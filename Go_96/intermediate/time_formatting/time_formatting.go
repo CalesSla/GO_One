07ed373a615eae43b19130da1f1aabd45d81080e
veaceslavcalestru@gmail.com <veaceslavcalestru@gmail.com>
2025-10-16 16:26:50 +0200

    feat: add time formatting examples with parsing for different layouts

---
+++ Go_96/intermediate/time_formatting/time_formatting.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	layout := "2006-01-2T15:04:05Z07:00"
+	str := "2024-07-04T14:30:18Z"
+
+	t, err := time.Parse(layout, str)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(t)
+
+	str1 := "Jul 03, 2024 03:18 PM"
+	layout1 := "Jan 02, 2006 03:04 PM"
+	t1, err := time.Parse(layout1, str1)
+	fmt.Println(t1)
+
+}
