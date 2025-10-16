9d4197aff0086dfaa9abcb11ccbb111934cb7b9f
veaceslavcalestru@gmail.com <veaceslavcalestru@gmail.com>
2025-10-16 16:21:28 +0200

    feat: add Unix time conversion example in epoch package

---
+++ Go_96/intermediate/epoch/epoch.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	// 00:00:00 UTC on January 1, 1970
+	now := time.Now()
+	unixTime := now.Unix()
+
+	fmt.Println("Current Unix time:", unixTime)
+
+	t := time.Unix(unixTime, 0)
+	fmt.Println("Converted time:", t)
+
+	fmt.Println("Time: ", t.Format("2006-01-02"))
+}
