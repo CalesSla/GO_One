996c0d84e1a408d1be65e62bc075338b80cdb4d6
veaceslavcalestru@gmail.com <veaceslavcalestru@gmail.com>
2025-10-16 12:05:37 +0200

    feat: add string manipulation examples using strings package

--- Go_96/intermediate/string_functions/string_functions.go
+++ Go_96/intermediate/string_functions/string_functions.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"strconv"
+	"strings"
 )
 
 func main() {
@@ -21,4 +22,33 @@ func main() {
 	num := 18
 	str3 := strconv.Itoa(num)
 	fmt.Println(str3)
+
+	fruits := "apple, orage, banana"
+	parts := strings.Split(fruits, ",")
+	fmt.Println(parts)
+
+	fmt.Printf("Type of parts: %T\n", parts)
+
+	countries := []string{"Germany", "France", "Italy"}
+	joined := strings.Join(countries, ", ")
+	fmt.Println(joined)
+
+	fmt.Println(strings.Contains(str, "World"))
+
+	replaced := strings.Replace(str, "World", "Go", 2)
+	fmt.Println(replaced)
+
+	strwspace := "   Hello World   "
+	fmt.Println("Before Trim:", strwspace)
+	fmt.Println("After Trim:", strings.TrimSpace(strwspace))
+
+	fmt.Println(strings.ToLower(strwspace))
+	fmt.Println(strings.ToUpper(strwspace))
+
+	fmt.Println(strings.Repeat("foo", 3))
+
+	fmt.Println(strings.Count(str, "l"))
+
+	fmt.Println(strings.HasPrefix("Hello", "he"))
+	fmt.Println(strings.HasSuffix("Hello", "lo"))
 }
