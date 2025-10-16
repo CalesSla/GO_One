1be075a02be4f59d3206c0173441bca365ec40a7
veaceslavcalestru@gmail.com <veaceslavcalestru@gmail.com>
2025-10-16 12:52:48 +0200

    feat: update string functions example to use strings.Builder for efficient string concatenation

--- Go_96/intermediate/string_functions/string_functions.go
+++ Go_96/intermediate/string_functions/string_functions.go
@@ -2,60 +2,80 @@ package main
 
 import (
 	"fmt"
-	"regexp"
-	"strconv"
 	"strings"
 )
 
 func main() {
 
-	str := "Hello World"
-	fmt.Println(len(str))
+	// str := "Hello World"
+	// fmt.Println(len(str))
 
-	str1 := "Hello"
-	str2 := "World"
-	result := str1 + " " + str2
+	// str1 := "Hello"
+	// str2 := "World"
+	// result := str1 + " " + str2
+	// fmt.Println(result)
+
+	// fmt.Println(str[0])
+	// fmt.Println(str[1:7])
+
+	// num := 18
+	// str3 := strconv.Itoa(num)
+	// fmt.Println(str3)
+
+	// fruits := "apple, orage, banana"
+	// parts := strings.Split(fruits, ",")
+	// fmt.Println(parts)
+
+	// fmt.Printf("Type of parts: %T\n", parts)
+
+	// countries := []string{"Germany", "France", "Italy"}
+	// joined := strings.Join(countries, ", ")
+	// fmt.Println(joined)
+
+	// fmt.Println(strings.Contains(str, "World"))
+
+	// replaced := strings.Replace(str, "World", "Go", 2)
+	// fmt.Println(replaced)
+
+	// strwspace := "   Hello World   "
+	// fmt.Println("Before Trim:", strwspace)
+	// fmt.Println("After Trim:", strings.TrimSpace(strwspace))
+
+	// fmt.Println(strings.ToLower(strwspace))
+	// fmt.Println(strings.ToUpper(strwspace))
+
+	// fmt.Println(strings.Repeat("foo", 3))
+
+	// fmt.Println(strings.Count(str, "l"))
+
+	// fmt.Println(strings.HasPrefix("Hello", "he"))
+	// fmt.Println(strings.HasSuffix("Hello", "lo"))
+
+	// str5 := "Hello, 123 Go 11!"
+	// re := regexp.MustCompile(`\d+`)
+	// matches := re.FindAllString(str5, -1)
+	// fmt.Println(matches)
+
+	// fmt.Println(utf8.RuneCountInString(str5))
+
+	var builder strings.Builder
+
+	builder.WriteString("Hello")
+	builder.WriteString(", ")
+	builder.WriteString("World")
+
+	result := builder.String()
 	fmt.Println(result)
 
-	fmt.Println(str[0])
-	fmt.Println(str[1:7])
+	builder.WriteRune(' ')
+	builder.WriteString("How are you")
 
-	num := 18
-	str3 := strconv.Itoa(num)
-	fmt.Println(str3)
+	result = builder.String()
+	fmt.Println(result)
 
-	fruits := "apple, orage, banana"
-	parts := strings.Split(fruits, ",")
-	fmt.Println(parts)
-
-	fmt.Printf("Type of parts: %T\n", parts)
-
-	countries := []string{"Germany", "France", "Italy"}
-	joined := strings.Join(countries, ", ")
-	fmt.Println(joined)
-
-	fmt.Println(strings.Contains(str, "World"))
-
-	replaced := strings.Replace(str, "World", "Go", 2)
-	fmt.Println(replaced)
-
-	strwspace := "   Hello World   "
-	fmt.Println("Before Trim:", strwspace)
-	fmt.Println("After Trim:", strings.TrimSpace(strwspace))
-
-	fmt.Println(strings.ToLower(strwspace))
-	fmt.Println(strings.ToUpper(strwspace))
-
-	fmt.Println(strings.Repeat("foo", 3))
-
-	fmt.Println(strings.Count(str, "l"))
-
-	fmt.Println(strings.HasPrefix("Hello", "he"))
-	fmt.Println(strings.HasSuffix("Hello", "lo"))
-
-	str5 := "Hello, 123 Go 11!"
-	re := regexp.MustCompile(`\d+`)
-	matches := re.FindAllString(str5, -1)
-	fmt.Println(matches)
+	builder.Reset()
+	builder.WriteString("Starting fresh")
+	result = builder.String()
+	fmt.Println(result)
 
 }
