3f9abc46c8f23f349eaad16301a3d997690109ce
veaceslavcalestru@gmail.com <veaceslavcalestru@gmail.com>
2025-10-17 13:26:33 +0200

    feat: update bufio example to demonstrate writing data to stdout with error handling

--- Go_96/intermediate/bufio/bufio.go
+++ Go_96/intermediate/bufio/bufio.go
@@ -3,26 +3,55 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"strings"
+	"os"
 )
 
 func main() {
 
-	reader := bufio.NewReader(strings.NewReader("Hello, bufio package!\n"))
+	// reader := bufio.NewReader(strings.NewReader("Hello, bufio package!\n"))
 
-	data := make([]byte, 20)
-	n, err := reader.Read(data)
+	// data := make([]byte, 20)
+	// n, err := reader.Read(data)
+	// if err != nil {
+	// 	fmt.Println("Error reading data:", err)
+	// 	return
+	// }
+	// fmt.Printf("Read %d bytes: %s\n", n, string(data[:n]))
+	// fmt.Println(string(data))
+
+	// line, err := reader.ReadString('\n')
+	// if err != nil {
+	// 	fmt.Println("Error reading line:", err)
+	// 	return
+	// }
+	// fmt.Printf("Read line: %s", line)
+
+	writer := bufio.NewWriter(os.Stdout)
+
+	data := []byte("Hello, bufio package!\n")
+	n, err := writer.Write(data)
+
 	if err != nil {
-		fmt.Println("Error reading data:", err)
+		fmt.Println("Error writing data:", err)
 		return
 	}
-	fmt.Printf("Read %d bytes: %s\n", n, string(data[:n]))
-	fmt.Println(string(data))
-
-	line, err := reader.ReadString('\n')
+	fmt.Printf("Wrote %d bytes.\n", n)
+	err = writer.Flush()
 	if err != nil {
-		fmt.Println("Error reading line:", err)
+		fmt.Println("Error flushing data:", err)
 		return
 	}
-	fmt.Printf("Read line: %s", line)
+
+	str := "This is a string.\n"
+	n, err = writer.WriteString(str)
+	if err != nil {
+		fmt.Println("Error writing string:", err)
+		return
+	}
+	fmt.Printf("Wrote %d bytes.\n", n)
+	err = writer.Flush()
+	if err != nil {
+		fmt.Println("Error flushing data:", err)
+		return
+	}
 }
