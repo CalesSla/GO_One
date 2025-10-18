24c43ed0e844846976e78a8fb3be8797f08d5fcf
veaceslavcalestru@gmail.com <veaceslavcalestru@gmail.com>
2025-10-18 18:05:01 +0200

    feat: add environment variable management and string splitting examples

---
+++ Go_96/intermediate/env_variables/env_variables.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+
+	user := os.Getenv("USER")
+	home := os.Getenv("HOME")
+
+	fmt.Println("User:", user)
+	fmt.Println("Home Directory:", home)
+
+	err := os.Setenv("FRUIT", "Apple")
+	if err != nil {
+		fmt.Println("Error setting environment variable:", err)
+		return
+	}
+
+	for _, e := range os.Environ() {
+
+		kvpair := strings.SplitN(e, "=", 2)
+		fmt.Println(kvpair[0])
+	}
+
+	fmt.Println("FRUIT:", os.Getenv("FRUIT"))
+	err = os.Unsetenv("FRUIT")
+	if err != nil {
+		fmt.Println("Error unsetting environment variable:", err)
+		return
+	}
+	fmt.Println("Unset env var on key FRUIT")
+
+	fmt.Println("FRUIT after unset:", os.Getenv("FRUIT"))
+
+	str := "a=b=c=d=e"
+
+	fmt.Println(strings.SplitN(str, "=", -1))
+	fmt.Println(strings.SplitN(str, "=", 0))
+	fmt.Println(strings.SplitN(str, "=", 1))
+	fmt.Println(strings.SplitN(str, "=", 2))
+	fmt.Println(strings.SplitN(str, "=", 3))
+	fmt.Println(strings.SplitN(str, "=", 4))
+	fmt.Println(strings.SplitN(str, "=", 5))
+
+}
