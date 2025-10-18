d72864f41e05530bd6d3d3e3269769a9fbb777db
veaceslavcalestru@gmail.com <veaceslavcalestru@gmail.com>
2025-10-18 19:30:34 +0200

    feat: add logging examples using Logrus and Zap libraries

---
+++ Go_96/intermediate/logging/logrus_logging/logrus_logging.go
@@ -0,0 +1,22 @@
+package main
+
+import "github.com/sirupsen/logrus"
+
+func main() {
+
+	log := logrus.New()
+
+	log.SetLevel(logrus.InfoLevel)
+
+	log.SetFormatter(&logrus.JSONFormatter{})
+
+	log.Info("This is an info message")
+	log.Warn("This is a warning message")
+	log.Error("This is an error message")
+
+	log.WithFields(logrus.Fields{
+		"username": "John Doe",
+		"method":   "GET",
+	}).Info("User logged in")
+
+}
